a13fee4e0bcbb7c2f63e01a52aa4607d287c28e9
chuongite <simane161@gmail.com>
2025-11-30 21:26:02 +0700

    Submit Bai Tap thuc hanh static method

---
+++ static-method/src/Student.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    //constructor to initialize the variable
+    public Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    //static method to change the value of static variable
+    public static void change() {
+        college = "CODEGYM";
+    }
+
+    //method to display values
+    public void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
